106cc44a393b4a027b7369de308ac323c4c1ec3f
Anwar Aji <96333744+anwarajijuloh@users.noreply.github.com>
2023-11-29 19:00:44 +0700

    final commit

---
+++ Assets/PlaceTrackedImages.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARTrackedImageManager))]
+public class PlaceTrackedImages : MonoBehaviour
+{
+    private ARTrackedImageManager _trackedImageManager;
+    public GameObject[] ArPrefabs;
+    private readonly Dictionary<string, GameObject> _instantiatedPrefabs = new Dictionary<string, GameObject>();
+    void Awake() {
+        _trackedImageManager = GetComponent<ARTrackedImageManager>();
+    }
+    void OnEnable() {
+        _trackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+    void OnDisable() {
+        _trackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+
+    private void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs){
+        foreach (var trackedImage in eventArgs.added)
+        {
+            var imageName = trackedImage.referenceImage.name;
+            foreach (var curPrefab in ArPrefabs)
+            {
+                if (string.Compare(curPrefab.name, imageName, StringComparison.OrdinalIgnoreCase) == 0 && !_instantiatedPrefabs.ContainsKey(imageName))
+                {
+                    var newPrefab = Instantiate(curPrefab, trackedImage.transform);
+                    _instantiatedPrefabs[imageName] = newPrefab;
+                }
+            }
+        }
+
+        foreach (var trackedImage in eventArgs.updated)
+        {
+            _instantiatedPrefabs[trackedImage.referenceImage.name].SetActive(trackedImage.trackingState == TrackingState.Tracking);
+        }
+
+        foreach (var trackedImage in eventArgs.removed)
+        {
+            Destroy(_instantiatedPrefabs[trackedImage.referenceImage.name]);
+            _instantiatedPrefabs.Remove(trackedImage.referenceImage.name);
+        }
+    }
+
+
+}
